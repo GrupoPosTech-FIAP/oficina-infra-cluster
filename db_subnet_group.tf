541d80c62c31eab474472a5b789d9b32afcbea6e
MatheusFuzari <matheus.fuzari004@gmail.com>
2026-08-19 22:08:56 -0300

    DB Infrastructure

--- db_subnet_group.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_db_subnet_group" "database" {
-  name       = "oficina-db-subnet-group"
-  subnet_ids = aws_subnet.subnet_public[*].id
-}
--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
+output "Tags" {
+  value = var.tags
+}
+
 output "VPC_CIDR_Block" {
   value = aws_vpc.vpc_oficina.cidr_block
 }
@@ -18,10 +22,10 @@ output "Repository_URL" {
   value = aws_ecr_repository.oficina_api.repository_url
 }
 
-output "DB_Endpoint" {
-  value = aws_db_instance.default.address
-}
-
 output "EKS_Cluster_Name" {
   value = aws_eks_cluster.oficina.name
+}
+
+output "EKS_Security_Group_Id" {
+  value = aws_eks_cluster.oficina.vpc_config[0].cluster_security_group_id
 }
